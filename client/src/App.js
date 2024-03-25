a07a70e5d09efe2f38b763e73e9bc6613e35ebc6
vuphuong1794 <phuongvu1794@gmail.com>
2024-03-25 14:27:45 +0700

    add register

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import Home from "./pages/Home/Home";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 import ProductList from "./pages/producList/ProductList";
 import SingleProduct from "./pages/SingleProduct/SingleProduct";
+import Register from "./pages/register/register";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/list" element={<ProductList />} />
         <Route path="/product" element={<SingleProduct/>}/>
+        <Route path="/register" element={<Register/>}/>
       </Routes>
     </BrowserRouter>
   );
