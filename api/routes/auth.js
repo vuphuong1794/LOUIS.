2668550c985e9cca3b3182c71b4e49981767d40e
vuphuong1794 <phuongvu1794@gmail.com>
2024-04-01 13:33:07 +0700

    fix authentication

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,21 +1,56 @@
 const router = require("express").Router();
-const User = require("../models/User")
-
-
+const User = require("../models/User");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 //register
-router.post("/register", async (req, res)=>{
-    const newUser = new User({
-        username: req.body.username,
-        email: req.body.email,
-        password: req.body.password
-    })
-    try{
-        const saveUser = await newUser.save()
-        res.status(201).json(saveUser)
-    }catch(err){
-        res.status(500).json(err)
-    }
-})
+router.post("/register", async (req, res) => {
+  try {
+    const salt = bcrypt.genSaltSync(10);
+    const hash = bcrypt.hashSync(req.body.password, salt);
 
-module.exports = router
+    const newUser = new User({
+      username: req.body.username,
+      email: req.body.email,
+      password: hash,
+    });
+    await newUser.save();
+    res.status(201).json("User has been created!");
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//login
+router.post("/login", async (req, res) => {
+  try {
+    const user = await User.findOne({
+      username: req.body.username,
+    });
+
+    !user && res.status(401).json("Wrong username!");
+
+    const isPasswordCorrect = await bcrypt.compare(
+      req.body.password,
+      user.password
+    );
+
+    if (!isPasswordCorrect) return next(createdError(400, "Wrong password!"));
+
+    const accessToken = jwt.sign(
+      {
+        id: user._id,
+        isAdmin: user.isAdmin,
+      },
+      process.env.JWT,
+      { expiresIn: "3d" }
+    );
+
+    const { password, ...others } = user._doc;
+    res.status(200).json({ ...others, accessToken });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
