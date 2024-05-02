b5f0a28ad27108f3f9fa2b7c87997c1224045bd1
vuphuong1794 <phuongvu1794@gmail.com>
2024-05-02 20:22:35 +0700

    update zalopayAPI

--- api/zalopay/server.js
+++ api/zalopay/server.js
@@ -3,7 +3,7 @@ const app = express();
 const axios = require("axios").default;
 const CryptoJS = require("crypto-js");
 const moment = require("moment");
-
+const qs = require("qs")
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -17,7 +17,7 @@ const config = {
 
 app.post("/payment", async (req, res) => {
   const embed_data = {
-    redirecturl:"https://pcrender.com"
+    redirecturl: "https://pcrender.com",
   };
 
   const items = [{}];
@@ -32,21 +32,96 @@ app.post("/payment", async (req, res) => {
     amount: 50000,
     description: `Lazada - Payment for the order #${transID}`,
     bank_code: "",
+    callback_url: "http://localhost:5000/callback",
   };
 
   // appid|app_trans_id|appuser|amount|apptime|embeddata|item
-  const data = config.app_id + "|" + order.app_trans_id + "|" + order.app_user + "|" + order.amount + "|" + order.app_time + "|" + order.embed_data + "|" + order.item;
-order.mac = CryptoJS.HmacSHA256(data, config.key1).toString();
+  const data =
+    config.app_id +
+    "|" +
+    order.app_trans_id +
+    "|" +
+    order.app_user +
+    "|" +
+    order.amount +
+    "|" +
+    order.app_time +
+    "|" +
+    order.embed_data +
+    "|" +
+    order.item;
+  order.mac = CryptoJS.HmacSHA256(data, config.key1).toString();
 
-try{
-  const result = await axios.post(config.endpoint, null, { params: order })
-  return res.status(200).json(result.data)    
-  
-}catch(err){
-console.log(err)}
+  try {
+    const result = await axios.post(config.endpoint, null, { params: order });
+    return res.status(200).json(result.data);
+  } catch (err) {
+    console.log(err);
+  }
 });
 
+app.post("/callback", (req, res) => {
+  let result = {};
 
-app.listen(5000, ()=>{
-    console.log("server is running on post 5000")
-})
+  try {
+    let dataStr = req.body.data;
+    let reqMac = req.body.mac;
+
+    let mac = CryptoJS.HmacSHA256(dataStr, config.key2).toString();
+    console.log("mac =", mac);
+
+    // kiểm tra callback hợp lệ (đến từ ZaloPay server)
+    if (reqMac !== mac) {
+      // callback không hợp lệ
+      result.return_code = -1;
+      result.return_message = "mac not equal";
+    } else {
+      // thanh toán thành công
+      // merchant cập nhật trạng thái cho đơn hàng
+      let dataJson = JSON.parse(dataStr, config.key2);
+      console.log(
+        "update order's status = success where app_trans_id =",
+        dataJson["app_trans_id"]
+      );
+
+      result.return_code = 1;
+      result.return_message = "success";
+    }
+  } catch (ex) {
+    result.return_code = 0; // ZaloPay server sẽ callback lại (tối đa 3 lần)
+    result.return_message = ex.message;
+  }
+
+  // thông báo kết quả cho ZaloPay server
+  res.json(result);
+});
+
+app.post("/order-status/:app_trans_id", async (req, res) => {
+  const app_trans_id = req.params.app_trans_id;
+  let postData = {
+    app_id: config.app_id,
+    app_trans_id: app_trans_id, // Input your app_trans_id
+  };
+
+  let data = postData.app_id + "|" + postData.app_trans_id + "|" + config.key1; // appid|app_trans_id|key1
+  postData.mac = CryptoJS.HmacSHA256(data, config.key1).toString();
+
+  let postConfig = {
+    method: "post",
+    url: "https://sb-openapi.zalopay.vn/v2/query",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+    data: qs.stringify(postData),
+  };
+
+  try {
+    const result = await axios(postConfig);
+    return res.status(200).json(result.data);
+  } catch (error) {
+    console.log(error.message);
+  }
+});
+app.listen(5000, () => {
+  console.log("server is running on post 5000");
+});
