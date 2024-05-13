5e102ca8fd0e2c7a7283cff35ee6f30d72be9a97
vuphuong1794 <phuongvu1794@gmail.com>
2024-05-13 14:52:50 +0700

    sendmail test3

--- api/routes/mailer.js
+++ api/routes/mailer.js
@@ -4,7 +4,7 @@ router.post("/sendMail", async(req, res)=>{
     const { username, email, subject, telephone, message } = req.body;
     try{
         const res = await sendMail(username, email, subject, telephone, message);
-        res.status(200).send(res);
+        res.status(200).send("email has send");
     }catch(err){
         res.status(500).json(err)
     }
