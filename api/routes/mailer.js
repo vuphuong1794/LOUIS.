935f013baaf52f9310630df964b7b24d01c5637f
vuphuong1794 <phuongvu1794@gmail.com>
2024-05-13 15:29:15 +0700

    fix sendmail bugs

--- api/routes/mailer.js
+++ api/routes/mailer.js
@@ -3,8 +3,8 @@ const sendMail = require("../sendMail")
 router.post("/sendMail", async(req, res)=>{
     const { username, email, subject, telephone, message } = req.body;
     try{
-        await sendMail(username, email, subject, telephone, message);
-        res.status(200).json("email has send");
+        const res = await sendMail(username, email, subject, telephone, message);
+        res.status(200).json(res);
     }catch(err){
         res.status(500).json(err)
     }
