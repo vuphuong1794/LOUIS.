8f78bd79f8702af238ffda838ebe3026f7a9d8e4
vuphuong1794 <phuongvu1794@gmail.com>
2024-08-27 10:47:46 +0700

    test

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -69,7 +69,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "https://louis-a89w.onrender.com/auth/google/callback",
+      callbackURL: "http://localhost:8000/auth/google/callback",
     },
     function (accessToken, refreshToken, profile, done) {
       done(null, profile);
